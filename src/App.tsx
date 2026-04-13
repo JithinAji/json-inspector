efb027ff786c37b863b736a39a56427e3042f507
Jithin Aji <jithinaji@Jithins-MacBook-Air.local>
2026-04-13 23:51:31 +0530

    add a demo set path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
+import createJSONEngine from './utils/json-engine.js'
+
+let initialValue = {
+  a: 1,
+  b: 3,
+  c: {
+    d: 2,
+    e: 4
+  }
+}
+
+function CreateSetInput({ onSet  }) {
+  const [path, setPath] = useState('')
+  const [value, setValue] = useState('')
+
+  return(
+    <div>
+      <input 
+        placholder="path (eg: a.b)" 
+        value={path}
+        onChange={(e) => setPath(e.target.value)}
+      />
+      <input 
+        placholder="value" 
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => onSet(path, value)}>
+        Set
+      </button>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const engineRef = useRef(createJSONEngine(initialValue))
 
+  const [, setTick] = useState(0)
+
+  const engine = engineRef.current
+
+  const handleSet = (path, value) => {
+    try {
+      engine.set(path, value)
+      setTick(t => t+1)
+    }
+    catch(e) {
+      console.log(e)
+    }
+  }
   return (
-    <h1>A json project by Jithin Aji</h1>
+    <div>
+      <CreateSetInput onSet={handleSet} />
+     <pre>{JSON.stringify(engine.state, null, 2)}</pre>
+    </div>
   )
 }
 
